ac3755cf28aee94a1e4c959e473b98f9b292b1b2
SainaaDotNet <46877765+MonDroids@users.noreply.github.com>
2025-06-23 19:33:44 +0800

    navBar aa zaslaa

--- App.jsx
+++ App.jsx
@@ -1,10 +1,17 @@
-import Header from "./components/header/Header";
-import "./components/header/Header.css";
-function App(){
-  return(
-    <>
+import Header from "./components/1.Header/Header";
+
+function App() {
+  return (
+    <div className="App">
       <Header />
-    </>
-  )
+      <div style={{ paddingTop: '80px' }}> {/* Header өндөртэй тэнцүү */}
+        <h1>Тавтай морил</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+        <p>Scroll down to see the sticky top navbar in action!</p>
+        <div style={{ height: '2000px' }}></div> {/* Scroll test */}
+      </div>
+    </div>
+  );
 }
+
 export default App;

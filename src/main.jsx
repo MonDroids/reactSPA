6c5c7bcacc3bb734d8578fc36ee9377053359317
SainDev <46877765+MonDroids@users.noreply.github.com>
2025-07-07 16:26:03 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<App/>}>
+        <Route path="/project1" element={<Project1 />} />
+        <Route path="/project2" element={<Project2 />} />
+        <Route path="/project3" element={<Project3 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
